c247e0e782f322f7a732f538ee7d370edafd3f6e
gerganadq <gerganaquievy@gmail.com>
2023-08-03 23:40:05 +0300

    Adding Firebase config

---
+++ React Project 1/src/config/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDJWbPZWZL0eHKJlXqrIdrFqNYqmNHmucI",
+    authDomain: "1st-react-project.firebaseapp.com",
+    projectId: "1st-react-project",
+    storageBucket: "1st-react-project.appspot.com",
+    messagingSenderId: "421846175867",
+    appId: "1:421846175867:web:4baf4e718bd175751c92de",
+    measurementId: "G-JDXRN06FKF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
