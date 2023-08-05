b4273ea38f7b6e2ffe04a388cb5a8a96679383ef
gerganadq <gerganaquievy@gmail.com>
2023-08-05 14:12:17 +0300

    Add Firebase databse

--- React Project 1/src/config/firebase-config.js
+++ React Project 1/src/config/firebase-config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,11 +12,14 @@ const firebaseConfig = {
     storageBucket: "1st-react-project.appspot.com",
     messagingSenderId: "421846175867",
     appId: "1:421846175867:web:4baf4e718bd175751c92de",
-    measurementId: "G-JDXRN06FKF"
+    measurementId: "G-JDXRN06FKF",
+    databaseURL: "https://st-react-project-d2c31-default-rtdb.europe-west1.firebasedatabase.app"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 export { app, analytics };
