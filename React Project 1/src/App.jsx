20f0485de75f5d10c8ea13a28e4271f7fdec65d3
gerganadq <gerganaquievy@gmail.com>
2023-08-07 18:26:33 +0300

    Add chakra UI for posts

--- React Project 1/src/App.jsx
+++ React Project 1/src/App.jsx
@@ -1,53 +1,72 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import * as ReactRouterDOM from 'react-router-dom';
-import { Route, Routes, Link} from 'react-router-dom';
-import { db } from './services/database-services.js';
-import { NewPost } from './views/NewPostForm/NewPostForm';
-import { PostList } from './components/PostList/PostList';
-import Home from './Pages/Home';
-import SignIn from './components/Authenthication/SignIn';
-import SignUp from './components/Authenthication/SignUp';
-import { PostPage } from './components/PostPage/PostPage';
-import Nav from './components/NavBar/NavBar';
-import { ChakraProvider } from '@chakra-ui/react'
-import Edit from './components/Authenthication/EditUser';
-import AuthenticationVer from './components/Authenthication/AuthenticationVer';
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import * as ReactRouterDOM from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom"
+import { db } from "./services/database-services.js"
+import { NewPost } from "./views/NewPostForm/NewPostForm"
+import { PostList } from "./components/PostList/PostList"
+import Home from "./Pages/Home"
+import SignIn from "./components/Authenthication/SignIn"
+import SignUp from "./components/Authenthication/SignUp"
+import { PostPage } from "./components/PostPage/PostPage"
+import Nav from "./components/NavBar/NavBar"
+import { ChakraProvider } from "@chakra-ui/react"
+import Edit from "./components/Authenthication/EditUser"
+import AuthenticationVer from "./components/Authenthication/AuthenticationVer"
+import PostsList from "./views/PostsList/PostList"
 function App() {
-
   async function forTest() {
     const newPost = {
       title: "New Post",
-      content: "This is the content of the new post."
-    };
-  
+      content: "This is the content of the new post.",
+    }
+
     // The 'push' method generates a unique key and stores the new post under that key
-    await db.push('posts', newPost);
+    await db.push("posts", newPost)
   }
 
   return (
     <>
-    <ChakraProvider>
-    <Nav/>
-    <AuthenticationVer/>
-    {/* <SignUp/> */}
-      <Routes>
-        <Route index element={<Home/>} />
-        <Route exact path="/new-post" element={<NewPost/>} />
-        <Route exact path="/post-list" element={<PostList/>} />
-        <Route exact path="/sign-in" element={<SignIn/>} />
-        <Route exact path="/sign-up" element={<SignUp/>} />
-        <Route exact path="/edit" element={<Edit/>} />
-        <Route exact path="/post-list/:id" element={<PostPage/>} />
-        {/* <Route exact path="/sign-out" element={<AuthenticationVer/>} /> */}
-      </Routes>
-     
-      </ChakraProvider> 
-    {/* <button onClick={forTest}>Test for adding post!</button> */}
+      <ChakraProvider>
+        <Nav />
+        <AuthenticationVer />
+        {/* <SignUp/> */}
+
+        <nav>
+          <ul>
+            <li>
+              <Link className="nav-links" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links" to="/new-post">
+                New Post
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links" to="/post-list">
+                Post List
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route exact path="/new-post" element={<NewPost />} />
+          <Route exact path="/post-list" element={<PostsList />} />
+          <Route exact path="/sign-in" element={<SignIn />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
+          <Route exact path="/edit" element={<Edit />} />
+          <Route exact path="/post-list/:id" element={<PostPage />} />
+          {/* <Route exact path="/sign-out" element={<AuthenticationVer/>} /> */}
+        </Routes>
+      </ChakraProvider>
+      {/* <button onClick={forTest}>Test for adding post!</button> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- React Project 1/src/components/PostList/PostList.jsx
+++ React Project 1/src/components/PostList/PostList.jsx
@@ -1,46 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { db } from '../../services/database-services';
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { db } from "../../services/database-services"
 
-export function PostList() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const postsData = await db.get('posts');
-        if (postsData) {
-            // Convert the postsData object to an array of posts
-            const postsArray = Object.keys(postsData).map((postId) => ({
-              id: postId,
-              ...postsData[postId],
-            }));
-            setPosts(postsArray);
-          }
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
-  return (
-    <div>
-      <h1>Post List</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={`/post-list/${post.id}`}>
-              <h3>{post.title}</h3>
-            </Link>
-            <p>{post.content}</p>
-            <p>{new Date(post.date).toLocaleString()}</p>
-            <p>{post.likes}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
+export function PostList() {}
