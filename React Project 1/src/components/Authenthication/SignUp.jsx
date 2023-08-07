7c2265acdaa0f971db46644bc26c6e9c9c129a23
Riaz Bhuiyan <mituselim82@gmail.com>
2023-08-07 08:34:40 +0300

    Minor SignUp fix

--- React Project 1/src/components/Authenthication/SignUp.jsx
+++ React Project 1/src/components/Authenthication/SignUp.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-
 import { useState } from 'react';
 import { createUserWithEmailAndPassword} from "firebase/auth";
 import { auth } from '../../config/firebase-config';
-
-
+import { db } from '../../services/database-services';
 
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const signUp = (e) => {
     e.preventDefault();
     
 
-  createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
+        const user = userCredential.user;
+        const userData = {
+          email: user.email,
+          username: username,
+        };
+
         console.log(userCredential);
+        console.log(userData);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
 
   return (
     <div className='sign-in-container'>
@@ -30,6 +37,7 @@ const SignUp = () => {
         <h1>Create account</h1>
         <input type='email' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type='password' placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type='text' placeholder='Enter your username' value={username} onChange={(e) => setUsername(e.target.value)} />
         <button type='submit'>Sign Up</button>
       </form>
     </div>
