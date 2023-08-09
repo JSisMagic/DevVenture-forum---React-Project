c53c43a8c3235b5cf1a79644d6c930fd4461ce86
Riaz Bhuiyan <mituselim82@gmail.com>
2023-08-09 17:48:59 +0300

    invisible Search

--- React Project 1/src/components/TagSearch/TagSearch.jsx
+++ React Project 1/src/components/TagSearch/TagSearch.jsx
@@ -1,27 +1,52 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom/dist';
+import { useNavigate } from 'react-router-dom';
+import { InputGroup, Input, InputRightElement, IconButton } from '@chakra-ui/react';
+import { SearchIcon, CloseIcon } from '@chakra-ui/icons';
 
 export function TagSearch() {
   const [searchTag, setSearchTag] = useState('');
-  const navigate = useNavigate()
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSearchIconClick = () => {
+    setIsSearchVisible(!isSearchVisible);
+    if (!isSearchVisible) {
+      setSearchTag('');
+    }
+  };
+
+  const handleClearSearch = () => {
+    setIsSearchVisible(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`/searched-tag/${searchTag}`);
+      setSearchTag('')
+    }
+  };
 
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <h2 style={{ marginRight: '10px' }}>Search Posts by Tag:</h2>
-        <input
-          type="text"
-          placeholder="Enter tag"
-          value={searchTag}
-          onChange={(e) => setSearchTag(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              // Navigate to the search results page with the entered tag
-              navigate(`/searched-tag/${searchTag}`);
-            }
-          }}
-        />
-      </div>
+      <InputGroup>
+        {isSearchVisible && (
+          <Input
+            type="text"
+            placeholder="Search by tag"
+            value={searchTag}
+            onChange={(e) => setSearchTag(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        )}
+        <InputRightElement>
+          <IconButton
+            icon={isSearchVisible ? <CloseIcon /> : <SearchIcon />}
+            onClick={isSearchVisible ? handleClearSearch : handleSearchIconClick}
+          />
+        </InputRightElement>
+      </InputGroup>
     </div>
   );
 }
+
+
