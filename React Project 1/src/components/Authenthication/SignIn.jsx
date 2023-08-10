9691baa4715dd2fc49b620bda99195c100b49edd
Riaz Bhuiyan <mituselim82@gmail.com>
2023-08-10 21:07:43 +0300

    AuthContext added

--- React Project 1/src/components/Authenthication/SignIn.jsx
+++ React Project 1/src/components/Authenthication/SignIn.jsx
@@ -4,19 +4,24 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../config/firebase-config';
 import { useNavigate } from 'react-router-dom';
 import { AuthenticationVer } from './AuthenticationVer';
+import { AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
 import './SignIn.css';
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { setUser } = useContext(AuthContext);
 
+    
   const signIn = (e) => {
     e.preventDefault();
-    
+      
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
+        setUser(userCredential.user); 
         navigate('/');
       })
       .catch((error) => {
