306ad2881071455b7b2e389e7013abb267ad563f
Riaz Bhuiyan <mituselim82@gmail.com>
2023-08-12 16:02:20 +0300

    comments counter fixed

--- React Project 1/src/services/comments.services.js
+++ React Project 1/src/services/comments.services.js
@@ -3,14 +3,16 @@ import { db } from "./database-services";
 export const getTotalCommentCount = async () => {
   const postsSnapshot = await db.get("posts");
   const postsSnapshotArray = Object.keys(postsSnapshot);
-  
+
   let totalCommentCount = 0;
 
   for (const postId of postsSnapshotArray) {
     const postSnapshotReplies = await db.get(`posts/${postId}/replies`);
-    const countRepliesInPost = Object.keys(postSnapshotReplies).length
-    totalCommentCount += countRepliesInPost
+    if (postSnapshotReplies) {
+      const countRepliesInPost = Object.keys(postSnapshotReplies).length;
+      totalCommentCount += countRepliesInPost;
+    }
   }
-  
+
   return totalCommentCount;
 };
