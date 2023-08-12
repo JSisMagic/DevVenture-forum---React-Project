145daeb569a546c1b852e9aff09192cea92cf93e
Riaz Bhuiyan <mituselim82@gmail.com>
2023-08-12 07:41:41 +0300

    search - dynamic like & sort implementation

--- React Project 1/src/components/TagSearchResults/TagSearchResults.jsx
+++ React Project 1/src/components/TagSearchResults/TagSearchResults.jsx
@@ -11,12 +11,23 @@ import {
   Button,
   HStack,
   IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
 import { FaThumbsUp } from "react-icons/fa";
+import { auth } from "../../config/firebase-config";
+import { useNavigate } from "react-router-dom";
 
 export function TagSearchResults() {
   const { tag: term } = useParams();
   const [filteredResults, setFilteredResults] = useState([]);
+  const [sortOption, setSortOption] = useState("newest");
+  const [sortedPosts, setSortedPosts] = useState([]);
+
+  const navigate = useNavigate();
+  const user = auth.currentUser;
 
   useEffect(() => {
     const handleSearch = async () => {
@@ -36,12 +47,7 @@ export function TagSearchResults() {
           )
         );
 
-        // Sort the filteredPosts array by date in descending order (newest to oldest)
-        const sortedPosts = filteredPosts.sort(
-          (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-        );
-
-        setFilteredResults(sortedPosts);
+        setFilteredResults(filteredPosts);
       } catch (error) {
         console.log(error.message);
       }
@@ -52,23 +58,92 @@ export function TagSearchResults() {
 
   const handleLike = async (postId) => {
     try {
-      // Update the likes count in the state
-      setFilteredResults((prevResults) =>
-        prevResults.map((postData) =>
-          postData.id === postId
-            ? { ...postData, likes: postData.likes + 1 }
-            : postData
-        )
-      );
+      if (!user) {
+        navigate("/sign-up");
+        return;
+      }
 
-      // Update the likes count in the database
-      await db.update(`posts/${postId}`, {
-        likes:
-          filteredResults.find((postData) => postData.id === postId).likes + 1,
-      });
+      const currentUserUID = user.uid;
+
+      // Find the post by ID
+      const likedPost = filteredResults.find((post) => post.id === postId);
+
+      if (likedPost) {
+        // Check if the post has a likedBy array and if the current user liked it
+        const userLiked =
+          likedPost.likedBy && likedPost.likedBy.includes(currentUserUID);
+
+        // Update the likedBy array based on userLiked
+        const updatedLikedBy = userLiked
+          ? likedPost.likedBy.filter((uid) => uid !== currentUserUID)
+          : likedPost.likedBy
+          ? [...likedPost.likedBy, currentUserUID]
+          : [currentUserUID]; // Initialize likedBy if it doesn't exist
+
+        // Increment or decrement the likes count based on userLiked
+        const updatedLikes = userLiked
+          ? likedPost.likes - 1
+          : likedPost.likes + 1;
+
+        // Update the likes count and likedBy array in the state
+        setFilteredResults((prevPosts) =>
+          prevPosts.map((post) =>
+            post.id === postId
+              ? { ...post, likes: updatedLikes, likedBy: updatedLikedBy }
+              : post
+          )
+        );
+
+        // Update the likes count and likedBy array in the database
+        await db.update(`posts/${postId}`, {
+          likes: updatedLikes,
+          likedBy: updatedLikedBy,
+        });
+      }
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  useEffect(() => {
+    let sortedPostsCopy = [...filteredResults];
+
+    switch (sortOption) {
+      case "newest":
+        sortedPostsCopy.sort((a, b) => new Date(b.date) - new Date(a.date));
+        break;
+      case "oldest":
+        sortedPostsCopy.sort((a, b) => new Date(a.date) - new Date(b.date));
+        break;
+      case "mostLiked":
+        sortedPostsCopy.sort(
+          (a, b) => b.likes - a.likes || new Date(b.date) - new Date(a.date)
+        );
+        break;
+      case "leastLiked":
+        sortedPostsCopy.sort(
+          (a, b) => a.likes - b.likes || new Date(b.date) - new Date(a.date)
+        );
+        break;
+      case "mostCommented":
+        sortedPostsCopy.sort(
+          (a, b) => (b.replies?.length || 0) - (a.replies?.length || 0)
+        );
+        break;
+      case "leastCommented":
+        sortedPostsCopy.sort(
+          (a, b) => (a.replies?.length || 0) - (b.replies?.length || 0)
+        );
+        break;
+      default:
+        break;
+    }
+
+    setSortedPosts(sortedPostsCopy);
+  }, [filteredResults, sortOption]);
+
+  const handleSort = (option) => {
+    setSortOption(option);
   };
 
   return (
@@ -78,7 +153,28 @@ export function TagSearchResults() {
         <p>No posts found for this tag.</p>
       ) : (
         <ul>
-          {filteredResults.map((postData) => (
+          <Menu>
+            <MenuButton as={Button} mb="20px">
+              Sort By: {sortOption}
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={() => handleSort("newest")}>Newest</MenuItem>
+              <MenuItem onClick={() => handleSort("oldest")}>Oldest</MenuItem>
+              <MenuItem onClick={() => handleSort("mostLiked")}>
+                Most Liked
+              </MenuItem>
+              <MenuItem onClick={() => handleSort("leastLiked")}>
+                Least Liked
+              </MenuItem>
+              <MenuItem onClick={() => handleSort("mostCommented")}>
+                Most Commented{" "}
+              </MenuItem>
+              <MenuItem onClick={() => handleSort("leastCommented")}>
+                Least Commented
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          {sortedPosts.map((postData) => (
             <li key={postData.id} className="searched-posts-container">
               <GlassContainer height="auto">
                 <Link
