24d64a2ee2d2c4974a8613a76d8a73f172f99d4f
gerganadq <gerganaquievy@gmail.com>
2023-08-13 19:59:25 +0300

    Add Lindedin, GitLab & GitHub links Member' profil

--- React Project 1/src/views/DetailedMember/DetailedMember.jsx
+++ React Project 1/src/views/DetailedMember/DetailedMember.jsx
@@ -1,19 +1,29 @@
-
-import { DownloadIcon } from "@chakra-ui/icons";
-import { Avatar, Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getUserById } from "../../services/users.services";
+import { DownloadIcon, EditIcon } from "@chakra-ui/icons"
+import { Avatar, Box, Button, Flex, Heading, IconButton, Text } from "@chakra-ui/react"
+import { useContext, useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { getUserById } from "../../services/users.services"
+import { AuthContext } from "../../context/AuthContext"
 
 const DetailedMember = () => {
-  const { id } = useParams();
-  const [memberData, setMemberData] = useState({});
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const { userData } = useContext(AuthContext)
+  const [memberData, setMemberData] = useState({})
 
   useEffect(() => {
     getUserById(id)
-      .then((data) => setMemberData(data))
-      .catch(console.error);
-  }, [id]);
+      .then(data => setMemberData(data))
+      .catch(console.error)
+  }, [id])
+
+  const handleShowEditModal = () => {
+    navigate("/edit")
+  }
+
+  const handleNavigate = url => {
+    window.open(url, "_blank", "noopener,noreferrer")
+  }
 
   return (
     <Box
@@ -24,7 +34,7 @@ const DetailedMember = () => {
       padding="2rem"
       borderRadius={10}
     >
-      <Flex justify="space-between" alignItems="center">
+      <Flex justify="space-between">
         <Flex gap={3}>
           <Avatar borderRadius="5px" size="2xl" src={memberData.imageURL} />
           <Box>
@@ -35,18 +45,23 @@ const DetailedMember = () => {
             <Heading size="md">dev</Heading>
           </Box>
         </Flex>
+        {userData?.username === memberData.username && (
+          <IconButton icon={<EditIcon />} onClick={handleShowEditModal} />
+        )}
       </Flex>
       <Box marginTop={5}>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo ipsa provident, tempora cum
-          dolorum pariatur porro ullam autem fugit omnis, sed qui. Natus sint sunt quaerat in
-          quibusdam pariatur voluptatibus?
-        </Text>
+        <Text>{memberData?.description}</Text>
       </Box>
       <Flex gap={2} marginTop={5}>
-        <Button>LinkedIn</Button>
-        <Button>GitLab</Button>
-        <Button>GitHub</Button>
+        {memberData?.linkedInURL && (
+          <Button onClick={() => handleNavigate(memberData?.linkedInURL)}>LinkedIn</Button>
+        )}
+        {memberData?.gitLabURL && (
+          <Button onClick={() => handleNavigate(memberData?.gitLabURL)}>GitLab</Button>
+        )}
+        {memberData?.gitHubURL && (
+          <Button onClick={() => handleNavigate(memberData?.gitHubURL)}>GitHub</Button>
+        )}
         <Button flexGrow={1}>
           <DownloadIcon marginRight={2} />
           CV
