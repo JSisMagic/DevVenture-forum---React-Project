e1f8032a7ab6747f961ab2cf627ee63bb1fbada3
gerganadq <gerganaquievy@gmail.com>
2023-08-16 19:53:10 +0300

    Fixing About Us page heading and content

--- React Project 1/src/components/AboutUs/AboutUs.jsx
+++ React Project 1/src/components/AboutUs/AboutUs.jsx
@@ -5,76 +5,52 @@ export const AboutUs = () => {
   return (
     <Box p={10} width="50%" marginInline="auto">
       <Heading as="h1" size="xl" mb={6} textAlign="center">
-      Nurturing the Dev Venture Community
+        Nurturing the Dev Venture Community
       </Heading>
       <Text>
-      Welcome to Dev Venture, your ultimate platform for developer empowerment. 
-      Our mission is to provide a dynamic ecosystem that nurtures talent, fosters growth, 
-      and connects developers with the opportunities they deserve.
+        Welcome to Dev Venture, your ultimate platform for developer empowerment.
+        Our mission is to provide a dynamic ecosystem that nurtures talent, fosters growth,
+        and connects developers with the opportunities they deserve.
       </Text>
-      <Heading size="lg" mb={2}>
-       Who We Are?
+      <Heading as="h2" size="md" mb={2}>
+      Who We Are?
       </Heading>
       <Text>
-      Dev Venture is not just a forum; it's a dynamic ecosystem designed exclusively
-      for developers to connect, collaborate, and catalyze innovation. We are a collective 
-      of tech enthusiasts, aspiring developers, and seasoned professionals who believe 
-      in the power of uniting minds, inspiring growth, and propelling the world of 
-      technology forward.
+        Dev Venture is not just a forum; it's a dynamic ecosystem designed exclusively
+        for developers to connect, collaborate, and catalyze innovation. We are a collective
+        of tech enthusiasts, aspiring developers, and seasoned professionals who believe
+        in the power of uniting minds, inspiring growth, and propelling the world of
+        technology forward.
       </Text>
-      <Heading size="lg" mb={2}>
+      <Heading as="h2" size="md" mb={2}>
       What We Do?
       </Heading>
       <Text>
-      At Dev Venture, we provide a platform that empowers developers to showcase their talents,
-      share ideas, and build a network that fuels creativity. We offer the tools to connect your
-      LinkedIn, GitHub, and GitLab profiles, making your accomplishments and skills accessible
-      to potential employers and collaborators. Our forum enables you to create posts, engage 
-      in discussions, and participate in an ecosystem that thrives on shared knowledge.
+        At Dev Venture, we provide a platform that empowers developers to showcase their talents,
+        share ideas, and build a network that fuels creativity. We offer the tools to connect your
+        LinkedIn, GitHub, and GitLab profiles, making your accomplishments and skills accessible
+        to potential employers and collaborators. Our forum enables you to create posts, engage
+        in discussions, and participate in an ecosystem that thrives on shared knowledge.
       </Text>
-      <Heading size="lg" mb={2}>
+      <Heading as="h2" size="md" mb={2}>
       Why We Do It?
       </Heading>
       <Text>
-      We are driven by a deep-seated belief that collaboration fuels progress. Dev Venture was born
-      out of the need to create a space where developers could come together, exchange ideas,
-      and explore new horizons. Our purpose is to foster an environment where tech enthusiasts 
-      can find inspiration, empowerment, and opportunities to advance their careers.
+        We are driven by a deep-seated belief that collaboration fuels progress. Dev Venture was born
+        out of the need to create a space where developers could come together, exchange ideas,
+        and explore new horizons. Our purpose is to foster an environment where tech enthusiasts
+        can find inspiration, empowerment, and opportunities to advance their careers.
       </Text>
-      <Heading size="lg" mb={2}>
+      <Heading as="h2" size="md" mb={2}>
       Our Mission
       </Heading>
       <Text>
-      Our mission is to unite developers from all walks of life and provide a nurturing ground for 
-      innovation and growth. We aim to inspire and empower you to elevate your skills, forge meaningful
-      connections, and contribute to the ever-evolving landscape of technology. Dev Venture strives 
-      to be the hub where networking, idea-sharing, and job opportunities converge.
+        Our mission is to unite developers from all walks of life and provide a nurturing ground for
+        innovation and growth. We aim to inspire and empower you to elevate your skills, forge meaningful
+        connections, and contribute to the ever-evolving landscape of technology. Dev Venture strives
+        to be the hub where networking, idea-sharing, and job opportunities converge.
       </Text>
-      <Heading size="lg" mb={2}>
-      Our Values: Unite. Inspire. Empower. Innovate.
-      </Heading>
-      <Text>
-      Unite: We bring developers together, breaking down barriers and fostering a sense of community
-      that transcends borders and backgrounds.
-      </Text>  
-      <Text>
-      Inspire: We aim to inspire individuals to push their boundaries, think creatively, 
-      and explore new solutions to complex challenges.    
-      </Text> 
-      <Text> Empower: Through knowledge-sharing and networking, we empower developers to take control
-      of their careers and make meaningful contributions.
-      </Text> 
-      <Text> Innovate: We encourage innovation by providing a platform where fresh ideas can flourish,
-      leading to the creation of groundbreaking technologies.
-      </Text> 
-      <Text> At Dev Venture, you can not only expand your professional horizons but also make a significant
-      impact on the tech world. Join us on this exciting journey where every connection, every post,
-      and every idea contributes to a thriving ecosystem of learning and progress. Together,
-      we are shaping the future of technology.
-      </Text>
-      <Text> Join Dev Venture today and become part of a vibrant community where inspiration meets innovation.
-      </Text>
-      
+    
       {/* Add more content about your team, mission, goals, etc. */}
     </Box>
   );
