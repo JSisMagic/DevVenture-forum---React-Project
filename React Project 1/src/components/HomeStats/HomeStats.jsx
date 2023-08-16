efc5461ab4bc0ead7fffdce18e6238bf8da4b7d3
gerganadq <gerganaquievy@gmail.com>
2023-08-16 16:16:45 +0300

    Refacturing HomeStats.jsx

--- React Project 1/src/components/HomeStats/HomeStats.jsx
+++ React Project 1/src/components/HomeStats/HomeStats.jsx
@@ -1,39 +1,39 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { getAllPosts } from "../../services/posts.services";
-import { getAllUsers } from "../../services/users.services";
-import { getTotalCommentCount } from "../../services/comments.services";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { getAllPosts } from "../../services/posts.services"
+import { getAllUsers } from "../../services/users.services"
+import { getTotalCommentCount } from "../../services/comments.services"
 
 const HomeStats = () => {
-  const [usersCount, setContextsCount] = useState(0);
-  const [postsCount, setPostsCount] = useState(0);
-  const [commentsCount, setCommentsCount] = useState(0);
+  const [usersCount, setContextsCount] = useState(0)
+  const [postsCount, setPostsCount] = useState(0)
+  const [commentsCount, setCommentsCount] = useState(0)
 
   useEffect(() => {
-    getAllUsers().then((data) => setContextsCount(data.length));
-    getAllPosts().then((data) => setPostsCount(data.length));
-    getTotalCommentCount().then((count) => setCommentsCount(count));
-  }, []);
+    getAllUsers().then(data => setContextsCount(data.length))
+    getAllPosts().then(data => setPostsCount(data.length))
+    getTotalCommentCount().then(count => setCommentsCount(count))
+  }, [])
 
   return (
     <Flex
-    padding={"10px"}
-    bg={"rgba(255, 255, 255, 0.1)"}
-    backdropBlur={"blur(26px)"}
-    alignItems={"center"}
-    boxShadow={"0rem 1rem 3rem rgba(0, 0, 0, 0.8)"}
-    borderRadius={"6px"}
-    opacity={"0.8"}
-    border={"none"}
-    color={"white"}
+      width="full"
+      padding={"16px"}
+      bg={"rgba(255, 255, 255, 0.1)"}
+      backdropBlur={"blur(26px)"}
+      justify="center"
+      alignItems={"center"}
+      boxShadow={"0rem 1rem 3rem rgba(0, 0, 0, 0.3)"}
+      borderRadius={"6px"}
+      opacity={"0.8"}
+      border={"none"}
+      color={"white"}
       gap={10}
       direction={"row"}
       // bg="rgba(255,255,255, 0.2)"
-      width="max-content"
       // padding="2rem"
       // borderRadius="lg"
-      maxW={"300px"}
-       textAlign="center"
+      textAlign="center"
       // marginInline="auto"
     >
       <Box>
@@ -49,7 +49,7 @@ const HomeStats = () => {
         <Heading>{commentsCount}</Heading>
       </Box>
     </Flex>
-  );
-};
+  )
+}
 
-export default HomeStats;
+export default HomeStats
